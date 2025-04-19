f5df682360e56f7303b8998dc4d41a5906532d0e
SAURINPATEL127 <saurin11292@gmail.com>
2025-04-19 11:04:30 -0400

    BaseClasses

--- src/main/java/com/test/driver/Driver.java
+++ src/main/java/com/test/driver/Driver.java
@@ -2,20 +2,28 @@ package com.test.driver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.Objects;
 
 public class Driver {
+    private Driver() {
+    }
 
     public static WebDriver driver;
 
     public static void setup()
     {
+        if(Objects.isNull(driver))
         WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     public static void tearDown(){
-        if(!Objects.isNull(driver))
-        driver.quit();
+        if(Objects.nonNull(driver))
+
+            driver.quit();
+        driver=null;
+
     }
 }
--- src/test/java/org/example/LoginTest.java
+++ src/test/java/org/example/LoginTest.java
@@ -1,22 +1,19 @@
 package org.example;
 
 import com.test.constants.FramworkConstants;
+import com.test.driver.Driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
 
-public class LoginTest extends BaseTest {
+public class LoginTest extends BaseTest{
 
-    WebDriver driver;
 
     @Test
-    public void test1()
+    public void send()
     {
-
-        driver = new ChromeDriver();
-        driver.get(FramworkConstants.getURL());
-
+        Driver.driver.get(FramworkConstants.getURL());
     }
 
 }
